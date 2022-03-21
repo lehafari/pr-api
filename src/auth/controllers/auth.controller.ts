4bc4fb14aeae5add8c91f58ff6e29903442b7b68
alejo <alejomedina161011@gmail.com>
2022-03-21 18:32:52 -0400

    201 to created

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Put } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/services/auth.service';
 import { CreateUserDto } from 'src/users/dto';
@@ -12,11 +12,13 @@ export class AuthController {
 
   @ApiBody({ type: CreateUserDto })
   @ApiOperation({ summary: 'Crear usuario' })
+  @HttpCode(201)
   @Put('signup')
   signup(@Body() createUserDto: CreateUserDto) {
     return this.authService.signup(createUserDto);
   }
   @ApiBody({ type: LoginDto })
+  @HttpCode(200)
   @ApiOperation({ summary: 'Autenticar Usuario' })
   @Post('login')
   login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
